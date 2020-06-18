fdeb753ced9d60d2b957bf24c104b4521fe2606d
Csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-06-18 08:54:59 -0500

    final

---
+++ README.md
@@ -0,0 +1,59 @@
+#  Servidor-Web-Concurrente
+## Manual de usuario
+
+Si se deseea hacer uso del programa lo primero que debe realizarse el clonar el repositorio almacenado en Github a través del siguiente comando:
+
+```
+git clone https://github.com/csarssj/ARSW-5-SpringApp/tree/master
+
+```
+O si desea puede descargarlo como archivo zip y luego descomprimirlo en la carpeta que desee.
+
+Una vez hecho alguno de los dos pasos anteriores, nos dirigimos a la ruta de instalación y por medio de la consola digitamos el siguiente comando:
+
+```
+mvnw spring-boot:run
+```
+ingresando a través de este link desplegado en heroku:
+
+[heroku](https://calm-brushlands-54526.herokuapp.com/index.html)
+
+o si prefiere directamente desde el localhost:
+
+[localhost](http://localhost:8080)
+
+este es un ejemplo de la app funcionando.
+
+![image](https://github.com/csarssj/ARSW-5-SpringApp/blob/master/img/prueba.png)
+
+### Prerequisitos
+
+Este proyecto necesita tener los siguientes progamas instalados en la máquina donde se deseea ejecutar:
+
+```
+  java version "1.8.0_251"
+  Apache Maven 3.6.3
+  git version 2.25.0.windows.1
+  jdk1.8.0_251
+```
+
+El sistema, mas alla de facilitar el registro de las iniciativas e ideas de proyectos, es una valiosa base de conocimiento donde los diferentes actores pueden revisar si hay iniciativas, ideas o intereses similares y aunar esfuerzos para la materializacion.
+
+
+
+## Construido en
+
+* [Maven](https://maven.apache.org/) - Dependency Management
+
+
+## Control de versiones y pruebas 
+
+[Github](https://github.com/) para el versionamiento.
+
+## Authors
+
+[César González](https://github.com/csarssj) 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/main/java/edu/escuelaing/edu/arsw/picasyfamas/PicasYfamas/logica/game.java
@@ -0,0 +1,66 @@
+package edu.escuelaing.edu.arsw.picasyfamas.PicasYfamas.logica;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class game {
+    
+        private List<String> attempts= new ArrayList<>();
+        int picas;
+        int famas;
+        int random;
+        String num;
+        
+        public game(){
+            picas = 0;
+            famas = 0;
+            num = null;
+            random = (int)(10000 * Math.random());
+            System.out.println(random);
+        }
+        
+        @GetMapping("/game")
+	public String print(Model model) {
+		model.addAttribute("picas",picas);
+                model.addAttribute("famas",famas);
+                model.addAttribute("random",random);                
+                model.addAttribute("attempts", attempts);
+		return "game";
+	}
+        @PostMapping("/game")
+        public String jugar(String currentAttempt) {
+            attempts.add(currentAttempt);
+            num = currentAttempt;
+            calc();
+            return "redirect:/game";
+        }
+        @RequestMapping(value = "/game", method = RequestMethod.POST, params = {"reset"})
+        public String reiniciar() {
+            attempts.clear();
+            famas = 0;picas=0;
+            random = (int)(10000 * Math.random());
+            return "redirect:/game";
+        }
+        public void calc(){
+            famas = 0;picas=0;
+            String ram = Integer.toString(random); 
+                for( int i = 0; i < num.length();i++){
+                    int pos = ram.indexOf(num.charAt(i));
+                    if(pos >=0){
+                        if(pos == i){
+                            famas++;
+                        }else{
+                            picas++;
+                        }
+                    }
+                }
+        }
+        
+}
