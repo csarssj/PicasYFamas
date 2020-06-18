7ecd166deac3bec613a66fbc3aaf78e9595a6797
Csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-06-18 01:25:40 -0500

    first

---
+++ src/main/java/edu/escuelaing/edu/arsw/picasyfamas/PicasYfamas/PicasYfamasApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.edu.arsw.picasyfamas.PicasYfamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicasYfamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicasYfamasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/edu/arsw/picasyfamas/PicasYfamas/PicasYfamasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.edu.arsw.picasyfamas.PicasYfamas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicasYfamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
